1b72094e4cfba48d54370cfc668541b16d0b82de
foxbei <foxbei@163.com>
2011-03-03 14:24:47 +0800

    date+

---
+++ sync.sh
@@ -0,0 +1,13 @@
+Dirname=basename $PWD
+echo "$Dirname haGithub sync begin ...... "
+echo "Begin exec: git add ."
+git add . 
+echo "Begin exec: git commit -a -m 'commit message'"
+echo -n "Please input $Dirname commit message: "
+read Msg
+Update='date'
+git commit -a -m $Update+$Msg
+echo "Begin exec: git push "
+echo "Pushing, please waiting ......"
+git push
+echo "$Dirname Github sync finished, exit. "
--- sync_mywiki.sh
+++
@@ -1,9 +0,0 @@
-echo "Github sync begin .. "
-echo "Begin exec: git add ."
-git add . 
-echo "Begin exec: git commit -a -m '%message'"
-git commit -a -m "wiki update"
-echo "Begin exec: git push "
-echo "Pleash waiting ......"
-git push
-echo "Github sync finished, exit."
